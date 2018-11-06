c96750875bc5489e755ea276ec0a064d92a4796a
Kevin Li <li.ctrl.z@gmail.com>
2018-11-05 23:22:12 -0800

    implemented code on partners computer and just copying working file to my own and pushing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var results = [];
+  var findElement = function(element) {
+    if(element.classList && element.classList.contains(className)) {
+      results.push(element);
+    }
+    if(element.childNodes) {
+      for(var i = 0; i < element.childNodes.length; i++) {
+        findElement(element.childNodes[i]);
+      }
+    }
+  }
+  findElement(document.body);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,110 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var str = '';
+  // case 0:
+  //   obj is null
+  //     -return "null"
+  if(obj === null) {
+    return 'null';
+  }
+
+  // else if (typeof (obj) === 'function') {
+  //   return;
+  // }
+  //
+  // else if (typeof(obj) === 'undefined') {
+  //   return;
+  // }
+  // case 1:
+  //   obj is boolean
+  //     -return "boolean"
+  else if(obj === true) {
+    return 'true';
+  }
+  else if(obj === false) {
+    return 'false';
+  }
+  //
+  // case 2:
+  //   obj is array
+  //     - start with "["
+  //   iterate through array
+  //     for each item
+  //       -if it is primitive value, add it to string
+  //       -if not, then call recursively
+  //       -if not last item, add ','
+  //       -if last item, add ']'
+  else if(Array.isArray(obj)) {
+    if(obj.length === 0) {
+      str += '[]';
+    } else {
+      str += '[';
+      for(var i = 0; i < obj.length; i++) {
+        if(typeof (obj[i]) === 'string') {
+          str += '"' + obj[i] + '"';
+        }
+        else if(typeof (obj[i]) === 'number') {
+          str += obj[i];
+        }
+        else {
+          str += stringifyJSON(obj[i]);
+        }
+        if(i !== obj.length-1) {
+          str += ',';
+        }
+      }
+      str += ']';
+    }
+  }
+  // case 3:
+  //   obj literal
+  //     -start with "{"
+  //     iterate through object
+  //       -add key into result string with '' and :
+  //       - if value is primitive, add it to strings
+  //       -if not, then call recursively
+  //       -if not last key, add ','
+  //       -if last key, add '}'
+  else if(typeof (obj) === 'object') {
+    console.log(obj);
+    str += '{';
+    if(Object.keys(obj).length === 0) {
+      str += '}';
+    }
+    for(var key in obj) {
+      if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+        str += '"' + key + '":';
+        if(typeof (obj[key]) === 'string') {
+          str += '"' + obj[key] + '"';
+        }
+        else if(typeof (obj[key]) === 'number') {
+          str += '"' + obj[key] + '"';
+        } else {
+          str += stringifyJSON(obj[key]);
+        }
+        if ((Object.keys(obj)[Object.keys(obj).length-1]) === key) {
+          str += '}';
+        } else {
+          str += ',';
+        }
+      } else {
+        if ((Object.keys(obj)[Object.keys(obj).length-1]) === key) {
+          str += '}';
+        }
+      }
+    }
+  }
+  // case 4:
+  //   primitive values
+  //     -add to string
+  else if(typeof (obj) === 'string'){
+    str += '"' + obj + '"';
+  }
+
+  else {
+    str += obj;
+  }
+
+  return str;
 };
